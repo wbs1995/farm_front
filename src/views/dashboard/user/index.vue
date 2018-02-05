1e6d5259eead30ffd42651f35b8af2b1b8606145
taoroot <jianroottao@gmail.com>
2018-02-05 12:55:48 +0800

    add vue-amap

--- src/views/dashboard/user/index.vue
+++ src/views/dashboard/user/index.vue
@@ -1,72 +1,81 @@
 <template>
-  <div class="dashboard-editor-container">
-    <div class=" clearfix">
-      <pan-thumb style="float: left" :image="avatar"> Your roles:
-        <span class="pan-info-roles" :key='item' v-for="item in roles">{{item}}</span>
-      </pan-thumb>
-      <div class="info-container">
-        <span class="display_name">{{name}}</span>
-        <span style="font-size:20px;padding-top:20px;display:inline-block;">editor : dashboard</span>
-      </div>
-    </div>
-    <div>
-      <img class="emptyGif" :src="emptyGif">
-    </div>
+  <div class="app-container">
+    <el-row :gutter="20">
+      <el-col :lg="16">
+        <el-card :body-style="mapCardStyle">
+          <el-amap vid="amapDemo" :features="features" class="amap-box">
+            <el-amap-marker vid="component-marker" :position="componentMarker.position"
+                            :content-render="componentMarker.contentRender"></el-amap-marker>
+          </el-amap>
+        </el-card>
+      </el-col>
+      <el-col :lg="8">
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>实时报警</span>
+            <el-button style="float: right; padding: 3px 0" type="text">刷新</el-button>
+          </div>
+          <div v-for="o in 4" :key="o" class="text item">
+            {{'列表内容 ' + o }}
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
-  import PanThumb from '@/components/PanThumb'
-
+  const exampleComponents = {
+    props: ['did'],
+    template: `<div>报警: {{did}}</div>`
+  }
   export default {
-    name: 'dashboard-editor',
-    components: { PanThumb},
+    name: 'amap-page',
     data() {
       return {
-        emptyGif: 'https://wpimg.wallstcn.com/0e03b7da-db9e-4819-ba10-9016ddfdaed3'
+        mapCardStyle: {
+          padding: '0px'
+        },
+        features: ['road', 'point'],
+        componentMarker: {
+          position: [119.738956, 29.114425],
+          contentRender: (h, instance) => h(exampleComponents, {
+            style: {backgroundColor: '#fff'},
+            props: {did: 't531w935952'}
+          })
+        }
       }
     },
-    computed: {
-      ...mapGetters([
-        'name',
-        'avatar',
-        'roles'
-      ])
-    }
+    methods: {}
   }
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-  .emptyGif {
-    display: block;
-    width: 45%;
-    margin: 0 auto;
+  .amap-box {
+    width: 100%;
+    height: 550px;
   }
 
-  .dashboard-editor-container {
-    background-color: #e3e3e3;
-    min-height: 100vh;
-    margin-top: -50px;
-    padding: 100px 60px 0px;
-    .pan-info-roles {
-      font-size: 12px;
-      font-weight: 700;
-      color: #333;
-      display: block;
-    }
-    .info-container {
-      position: relative;
-      margin-left: 190px;
-      height: 150px;
-      line-height: 200px;
-      .display_name {
-        font-size: 48px;
-        line-height: 48px;
-        color: #212121;
-        position: absolute;
-        top: 25px;
-      }
-    }
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 100%;
+    height: 350px;
   }
 </style>
